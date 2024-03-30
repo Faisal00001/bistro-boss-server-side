a2c9e2274de63ce3b8897bac2abeff5cb1e6412c
Faisal00001 <faisalosman798@gmail.com>
2024-03-30 12:50:33 +0600

    Added user post api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhjpjbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,12 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
     
+        // users related api
+        app.get('/users',  async (req, res) => {
 
+          const result = await userCollection.find().toArray();
+          res.send(result);
+        });
+        app.post('/users', async (req, res) => {
+          const user = req.body;
+          // insert email if user doesnt exists: 
+          // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+          const query = { email: user.email }
+          const existingUser = await userCollection.findOne(query);
+          if (existingUser) {
+            return res.send({ message: 'user already exists', insertedId: null })
+          }
+          const result = await userCollection.insertOne(user);
+          res.send(result);
+        });
         
     // menu related apis
     app.get('/menu', async (req, res) => {
